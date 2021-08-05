133a4c183a1a5eacfbe6d8e6437bb4293462de62
layne <layne@it.com>
2021-08-05 23:54:37 +0800

    hot-fix commit1

--- src/main/java/com/it/GitTest.java
+++ src/main/java/com/it/GitTest.java
@@ -5,6 +5,9 @@ public class GitTest {
 
         System.out.println("gitTest");
         System.out.println("git 1");
+        System.out.println("git 2");
+        System.out.println("git 3");
+
 
     }
 }
